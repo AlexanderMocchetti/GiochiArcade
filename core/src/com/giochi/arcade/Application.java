440c3fa0d6e9e200ad98cca9a4ee8d7b2eed7fb3
alexandermocchetti <mocchettialexander@gmail.com>
2023-04-17 19:03:48 +0200

    Tutorial

--- core/src/com/giochi/arcade/Application.java
+++ core/src/com/giochi/arcade/Application.java
@@ -1,34 +1,38 @@
 package com.giochi.arcade;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class Application extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	Player player;
-	
+	ShapeRenderer shape;
+	Random random = new Random();
+	ArrayList<Ball> balls = new ArrayList<>(10);
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("Player.png");
-		player = new Player(img);
+		shape = new ShapeRenderer();
+		for (int i = 0; i < 10; i++) {
+			balls.add(new Ball(
+					random.nextInt(Gdx.graphics.getWidth()),
+					random.nextInt(Gdx.graphics.getHeight()),
+					random.nextInt(100),
+					random.nextInt(15),
+					random.nextInt(15)
+			));
+		}
 	}
-
 	@Override
 	public void render () {
-		ScreenUtils.clear(Color.BLACK);
-		batch.begin();
-		player.draw(batch);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		shape.begin(ShapeRenderer.ShapeType.Filled);
+		for(Ball ball: balls){
+			ball.update();
+			ball.draw(shape);
+		}
+		shape.end();
 	}
 }
