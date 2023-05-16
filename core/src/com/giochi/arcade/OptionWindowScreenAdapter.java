2b6bb7b44b430a1bebea6d2fc1e317be56f23cbf
AmStanDem <96748673+AmStanDem@users.noreply.github.com>
2023-05-16 13:18:23 +0200

    f

--- core/src/com/giochi/arcade/OptionWindowScreenAdapter.java
+++ core/src/com/giochi/arcade/OptionWindowScreenAdapter.java
@@ -1,9 +1,12 @@
 package com.giochi.arcade;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.VerticalGroup;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
@@ -14,6 +17,8 @@ public class OptionWindowScreenAdapter extends ScreenAdapter
     private Viewport viewport;
 
     private Camera camera;
+
+    private VerticalGroup group;
 
     private static int WORLD_WIDTH = 640;
 
@@ -28,6 +33,14 @@ public class OptionWindowScreenAdapter extends ScreenAdapter
         viewport = new FitViewport(WORLD_WIDTH , WORLD_HEIGHT , camera);
 
         stage = new Stage();
+
+        Gdx.input.setInputProcessor(stage); // rendo lo stage , e di conseguenza la finestra pronta a ricevere degli input.
+
+        camera.update();
+
+        group = new VerticalGroup();
+
+        group.setFillParent(true); // diventa il "genitore" dei componenti a lui collegati
     }
 
     @Override
@@ -38,6 +51,7 @@ public class OptionWindowScreenAdapter extends ScreenAdapter
     @Override
     public void resize(int width, int height) {
         super.resize(width, height);
+
         viewport.update(width , height , true);
     }
 }
