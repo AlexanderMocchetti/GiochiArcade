aeeda73e1678fa215f6f67a5227f15f749779d88
AmStanDem <96748673+AmStanDem@users.noreply.github.com>
2023-05-18 09:58:01 +0200

    Versione con giochi di Riotto

--- core/src/com/giochi/arcade/ArcadeGame.java
+++ core/src/com/giochi/arcade/ArcadeGame.java
@@ -4,9 +4,26 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.ScreenAdapter;
 
 public class ArcadeGame extends Game {
+
+    private static ArcadeGame game = null;
+
+    public static ArcadeGame getInstance()
+    {
+        if (game == null)
+        {
+            game = new ArcadeGame();
+        }
+        return game;
+    }
+
+    private ArcadeGame()
+    {
+        create();
+    }
+
     @Override
     public void create() {
-
+        setScreen(new StartScreen());
     }
 
     public void setNewScreen(ScreenAdapter newScreen)
